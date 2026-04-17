3b1ce6a358b39d582d387b30b3784ff23fed2bec
rfahur11 <rfahrur6045@gmail.com>
2026-04-17 08:37:32 +0700

    test

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -12,7 +12,7 @@ def main():
     y = df['Churn']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    print("Melatih ulang model untuk Production...")
+    print("Melatih ulang model untuk Production ...")
     rf = RandomForestClassifier(n_estimators=100, random_state=42)
     rf.fit(X_train, y_train)
 
